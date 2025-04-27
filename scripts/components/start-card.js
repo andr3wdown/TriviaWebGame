2d6170ffaf0be0671fdcd5b8cb6745fc5a1eaae7
Chong Lee <upgamesemail@gmail.com>
2025-04-28 00:55:45 +0300

    updated start card text

--- scripts/components/start-card.js
+++ scripts/components/start-card.js
@@ -2,7 +2,7 @@ export class StartCard extends HTMLElement {
     template(){
         return /*html*/`
         <div class="flex flex-col grow">
-            <h1 class="my-4 text-3xl lg:text-4xl lg:my-10">Welcome to the trivia game!</h1>
+            <h1 class="my-4 text-3xl lg:text-4xl lg:my-10">Welcome to Andr3wdowns trivia game!</h1>
             <div class="container grow"></div>
             <div class="flex flex-col justify-center grid-cols-1 m-10 space-y-4 align-bottom lg:space-y-0 lg:grid-cols-2 grow lg:grow-0">
                 <input type="text" id="name-input" placeholder="Enter your name" class="flex text-center items-center justify-center transition-colors grow min-h-16 lg:m-4 bg-pink-700/40 backdrop-contrast-200 hover:bg-pink-500/40 lg:h-32 text-pink-50 rounded-xl">
